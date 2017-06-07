b0a97b107d04fc45966da49ec79f76ebf3f879f6
Zack Rubenstein <zrubenst@gmail.com>
2017-06-07 19:34:31 -0400

    Initial Commit

---
+++ MonoRail/MonoRail.h
@@ -0,0 +1,19 @@
+//
+//  MonoRail.h
+//  MonoRail
+//
+//  Created by Zack Rubenstein on 6/7/17.
+//  Copyright © 2017 ZRUBENST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MonoRail.
+FOUNDATION_EXPORT double MonoRailVersionNumber;
+
+//! Project version string for MonoRail.
+FOUNDATION_EXPORT const unsigned char MonoRailVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MonoRail/PublicHeader.h>
+
+
